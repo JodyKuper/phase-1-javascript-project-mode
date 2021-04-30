1520fdea3373dd06e67df08f76602eb1b15cd08f
Joel Cooper <jodycooper16@gmail.com>
2021-04-30 12:18:13 -0700

    almost done

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ const recpClick= document.getElementById('search-recipe')
 const recipeName= document.getElementById('search-form-recipe')
 const mealHolder= document.getElementById('meal-holder')
 const recipeHolder= document.getElementById('recipe-holder')
-const ingredientHolder= document.getElementsByClassName('ingredient-holder')
+const ingredientHolder= document.getElementById('ingredient-holder')
+let MEAL_ARRAY = []
 
 const fetchByName = () => { 
     // debugger
     event.preventDefault()
+    resetPage()
     fetch(Meal_URL +`?s=${recipeName[0].value}`)
     .then ((res)=> res.json())
     .then((data) => {
         // debugger
         data.meals.forEach((recipe)=> {
-          recipeList(recipe)  
+          recipeListItem(recipe)
+          MEAL_ARRAY.push(recipe) 
     })     
     })
 }
@@ -33,11 +36,13 @@ fetch(Area_URL +`?a=${dropDown.value}`)
 })
 }
 
-const recipeList =(recipe) => {
+const recipeListItem  =(recipe) => {
+    // resetPage()
     const newRecipe= document.createElement('li')
     newRecipe.innerText=recipe.strMeal
     // console.log(recipe)
     recipeHolder.appendChild(newRecipe)
+    newRecipe.addEventListener('click', ingredientList )
 }
 
 
@@ -46,26 +51,43 @@ const mealList = (cuisines) => {
     const newMeal = document.createElement('li')
     newMeal.innerText=cuisines.strMeal
     mealHolder.appendChild(newMeal)
+    newMeal.addEventListener('click', ingredientList)
 
 }
 
-const ingredient =(recipe)=> {
-    // debugger
-    data.strIngredient1.forEach((recipe) => {
-        ingredient(recipe)
-    })
-    const theIng = document.createElement(li)
-    theIng.innerText=recipe.strIngredient1
-    ingredientHolder.appendChild(theIng)
-
-}
-
-
+const findMeal= mealTerm => {
+    return MEAL_ARRAY.find(e => e.strMeal === mealTerm)
+    }
+const ingredientList =(e)=> {
+    const mealTerm = e.target.innerText
+   
+    const theMeal = findMeal(mealTerm)
     
+    if (!theMeal.ingredientArr) {
+        let ingredientArr = [];
+        for(let i=1; i<=20; ++i) {
+        const ingredientString = theMeal[`strMeasure${i}`] +" "+  theMeal[`strIngredient${i}`]
+        ingredientArr.push(ingredientString)        
+    }    
+      
+     theMeal['ingredientArr']=ingredientArr 
+}
+displayIngredients(theMeal)
+
+}
+  
+const displayIngredients = (theMeal) => {
+    const {quanity, ingredient} = theMeal
+    const newDisplay = document.createElement('hi')
+    newDisplay.innerText = theMeal.ingredientArr
+    ingredientHolder.appendChild(newDisplay)
+    console.log(newDisplay)
+}
+
+const resetPage = () => {
+   recipeHolder.innerText= "" 
+} 
 
 dropDown.addEventListener('click',fetchByCuisine)
 recipeName.addEventListener('submit',fetchByName)
-// fetchByName()
-// fetchByCuisine()
-// recipeList()
-// mealList()
+
